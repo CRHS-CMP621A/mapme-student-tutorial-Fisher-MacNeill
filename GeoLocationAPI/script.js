171cd3161be74a84eb6c4b65493b0070d05caf8a
macneillfisher-cloud <encryptorjr2@gmail.com>
2026-05-19 10:01:25 -0300

    I have added the map and got the form showing to work.

---
+++ GeoLocationAPI/script.js
@@ -0,0 +1,24 @@
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    // console.log(position);
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    console.log(`https://www.google.com/maps/@${46.4019},${-63.78}`);
+    const coords = [latitude, longitude];
+
+    var map = L.map("map").setView(coords, 13);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup("A pretty CSS popup.<br> Easily customizable.")
+      .openPopup();
+  },
+  function () {
+    alert("Could not get position.");
+  }
+);
--- MapApp/script.js
+++ MapApp/script.js
@@ -1,14 +1,60 @@
-'use strict';
+"use strict";
 
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
 
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    // console.log(position);
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coords = [latitude, longitude];
 
+    var map = L.map("map").setView(coords, 13);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup(
+        "Sir Andrew John Joseph Tennison Bevan Baker Demitrius Demarcus James III Jr"
+      )
+      .openPopup();
+
+    map.on("click", function (mapEvent) {
+      const lat = mapEvent.latlng.lat;
+      const lng = mapEvent.latlng.lng;
+      console.log(mapEvent);
+      form.classList.remove(`hidden`);
+      inputDistance.focus();
+      L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+          L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: "running-popup",
+          })
+        )
+        .setPopupContent(`Workout <img src=alexjones-alex-jones-jew.gif>`)
+        .openPopup();
+    });
+  },
+
+  function () {
+    alert("Could not get position.");
+  }
+);
